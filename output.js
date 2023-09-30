eab9557381b2d5147fac4502cb472e3631d560b7
Md.Muzahidul Islam <muzahidulislam.munna75@gmail.com>
2023-10-01 01:25:45 +0600

    javascript introduction

---
+++ output.js
@@ -0,0 +1,4 @@
+document.getElementById('demo').innerHTML=6+6;
+document.write(6+6);
+alert(6+6);
+console.log("hello world");
---
+++ syntext.js
@@ -0,0 +1,4 @@
+var a,b;
+a=5;
+b=5;
+document.getElementById('demo').innerHTML= 10+10 ;
