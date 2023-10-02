e2cfcad22b32739640ef5adfc7760db4e06d2141
Md.Muzahidul Islam <muzahidulislam.munna75@gmail.com>
2023-10-03 01:26:58 +0600

    function and object

---
+++ function.js
@@ -0,0 +1,7 @@
+function myfunction(n1,n2){
+    return n1*n2;
+
+}
+let x = myfunction(3,4);
+document.getElementById("demo1").innerHTML= x;
+
---
+++ object.js
@@ -0,0 +1,17 @@
+const car={
+    firstName:"BMW",
+    lastName:"odi",
+    start : function(){
+        console.log("car  started");
+    },
+    model : function(){
+        console.log("car is driving");
+    }
+  
+}
+car.start();
+
+var price1=5;
+var price2=5;
+var total=price1+price2;
+console.log("The value of a is :" +total);
